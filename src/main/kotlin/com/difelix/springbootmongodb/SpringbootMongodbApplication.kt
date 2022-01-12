f18a273cac48a0ebc85d6f8cbfa5c09375fde94d
difelix <dfelixsilva08@gmail.com>
2022-01-12 16:03:09 -0300

    commit inicial do projeto

---
+++ src/main/kotlin/com/difelix/springbootmongodb/SpringbootMongodbApplication.kt
@@ -0,0 +1,11 @@
+package com.difelix.springbootmongodb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootMongodbApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootMongodbApplication>(*args)
+}
